ae90b0df23682d115e9ce4a4e5b00beffbfcb277
Nehaarya9520 <nehaarya2093@gmail.com>
2025-04-16 14:24:35 +0530

    update

--- fashion/urls.py
+++ fashion/urls.py
@@ -19,7 +19,5 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="index.html"), name="home" ),
-    path('fashion/', include("fashion.urls"), name="fashion" ),#import all files which is create in App
 ]
