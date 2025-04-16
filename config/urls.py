57c3a506554a76a32ebbc462282a3eb7dfbc6a1b
Jaya-7007 <jaya313kumari@gmail.com>
2025-04-16 14:14:23 +0530

    bdshfbd

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="index.html"), name="home" ),
+    path('login/', TemplateView.as_view(template_name="login.html"), name="login" ),
+    path('', TemplateView.as_view(template_name="user_page.html"), name="profile" ),
+    path('', TemplateView.as_view(template_name="history.html"), name="history" ),
 ]
