0bd364f7ccbf44221e47fc100d0e45199c7025f6
m-triple-m <triplem656@gmail.com>
2025-05-17 16:33:17 +0530

    added search

--- fashion/urls.py
+++ fashion/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from .views import searchVideo, viewVideos
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html"), name="home" ),
+    path('search/', searchVideo, name='search_video'),
+    path('results/', viewVideos, name='view_videos'),
 ]
--- fashion/views.py
+++ fashion/views.py
@@ -1,5 +1,46 @@
 from django.shortcuts import render
+import http.client
+import json
+from .socialScrapper import get_all_video_urls
 
 # Create your views here.
 def searchVideo(request):
+    return render(request, 'search.html')
+
+def viewVideos(request):
+    if request.method == 'POST':
+        hashtag = request.POST.get('hashtag', '')
+        if not hashtag:
+            return render(request, 'result.html', {'error': 'Please provide a hashtag'})
+        
+        try:
+            # Connect to the Instagram API
+            conn = http.client.HTTPSConnection("instagram-scraper-stable-api.p.rapidapi.com")
+            
+            headers = {
+                'x-rapidapi-key': "d30c9b20a1mshf7b0cf45d9eb8c4p1b1d51jsn7724536d9cb5",
+                'x-rapidapi-host': "instagram-scraper-stable-api.p.rapidapi.com"
+            }
+            
+            conn.request("GET", f"/search_hashtag.php?hashtag={hashtag}", headers=headers)
+            
+            res = conn.getresponse()
+            data = res.read()
+            
+            # Parse the JSON response
+            og_data = data.decode("utf-8")
+            og_data_dict = json.loads(og_data)
+            
+            # Get video URLs using the helper functions
+            video_urls = get_all_video_urls(og_data_dict)
+            
+            return render(request, 'result.html', {
+                'video_urls': video_urls,
+                'hashtag': hashtag,
+                'count': len(video_urls)
+            })
+            
+        except Exception as e:
+            return render(request, 'result.html', {'error': f'Error fetching videos: {str(e)}'})
+    
     return render(request, 'search.html')
